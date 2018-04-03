1fd83970ddf6f6f64e5adfadd906e10d6fd0259f
ch1nch1ll4 <th3ch1nch1ll4@gmail.com>
2018-04-03 22:02:10 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Tyson',
-    text: 'Hey. I am coming for you.'
-  });
 });
 
 socket.on('disconnect', function () {
