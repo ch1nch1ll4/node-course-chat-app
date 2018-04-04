dfe64024e07d443813841aaabe18bbd5d7840613
ch1nch1ll4 <th3ch1nch1ll4@gmail.com>
2018-04-04 14:07:38 +0200

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -14,27 +14,33 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  var template = jQuery('#message-template').html();
 
-  jQuery('#messages').append(li);
+  var html = Mustache.render(template, { //rendering template dynamic way
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
 });
 
 
 socket.on('newLocation', function (location) {
   var formattedTime = moment(location.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
+  var template = jQuery('#location-template').html();
 
-  li.text(`${location.from} ${formattedTime}: `);
-  a.attr('href', location.url);
-  li.append(a);
+  var html = Mustache.render(template, {
+    from: location.from,
+    url: location.url,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 
-//listeners-------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
