6b8dccc7057e06155a272e6210c18568eab1bc43
ch1nch1ll4 <th3ch1nch1ll4@gmail.com>
2018-04-04 15:00:06 +0200

    Scroll to bottom if user close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 var socket = io();
 
 
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages')
+  var newMessage = messages.children('li:last-child')
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop +newMessageHeight +lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+
 //------------------------------------------------------------------------------
 socket.on('connect', function () {
   console.log('Connected to server');
@@ -23,6 +40,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 
@@ -37,6 +55,7 @@ socket.on('newLocation', function (location) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 
